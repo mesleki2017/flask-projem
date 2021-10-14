8181d9940c0da139640b6664cbca5a5455759520
mesleki2017 <mesleki2017@gmail.com>
2021-10-14 16:13:21 +0300

    websocket deneme

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-
 from flask_socketio import SocketIO
 from geventwebsocket.handler import WebSocketHandler
 from gevent.pywsgi import WSGIServer
 from flask import Flask, request, render_template
 import math
+import threading
 
+sanal_data={
+    "x":0,
+    "y":0
+}
 
 
 
@@ -13,10 +17,19 @@ app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 
 socketio = SocketIO(app)
 
+def thread1():
+    threading.Timer(0.5,thread1).start()
+    sanal_data["x"]=sanal_data["x"]+1
+    sanal_data["y"]=math.sin(sanal_data["x"])
+    socketio.emit('server_data', sanal_data)
+
+thread1()
+
 @app.route('/')#decorator
 def sessions():
     #flask server calistiginda server adresinde bu sayfa acilir
     return render_template('index.html')
+
 
 
 if __name__ == '__main__':
--- static/javascript/grafik1.js
+++ static/javascript/grafik1.js
@@ -1,5 +1,5 @@
 const data1 = {"x":[],"y":[]};
-const data2 = {"x":[],"y":[]};
+
 
 chart1_data= {
       labels: data1["x"],
@@ -23,24 +23,3 @@ chart1_options={
       }
     }
 
-chart2_data= {
-        labels: data2["x"],
-        datasets: [{
-          label :"cos",
-          pointRadius: 4,
-          pointBackgroundColor: "rgb(0,0,255)",
-          data: data2["y"]
-        }]
-      }
-chart2_options={
-        scaleOverride: true,
-          scaleSteps: 10,
-          scaleStepWidth: 10,
-          scaleStartValue: 0,
-        legend: {display: true},
-        responsive: true,
-        title:{display: true,text:"MyChart2"},
-        scales: {
-        //Number - The scale starting value
-        }
-      }
