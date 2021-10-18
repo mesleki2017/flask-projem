2c02ce09aaf11e028ad94e44ffd31885aeade047
mesleki2017 <mesleki2017@gmail.com>
2021-10-18 15:50:01 +0300

    ok

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask_socketio import SocketIO
-from geventwebsocket.handler import WebSocketHandler
-from gevent.pywsgi import WSGIServer
-from flask import Flask, request, render_template
+from fastapi import Request
+from fastapi.staticfiles import StaticFiles
+import json
+import asyncio
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi import WebSocket
+from fastapi.templating import Jinja2Templates
+import uvicorn
+from pathlib import Path
+import numpy as np
 import math
-import random
-import threading
+
+
 
 datam1={
     "x":0,
@@ -15,27 +22,24 @@ def sanal_data(data):
     data["x"]=data["x"]+1
     data["y"]=math.sin(data["x"])
 
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
-
-socketio = SocketIO(app)
-
-def thread1(data):
-    threading.Timer(0.1,thread1,[data]).start()
-    sanal_data(data)
-    socketio.emit('server_data', data)
-    
-thread1(datam1)
 
 
-@app.route('/')#decorator
-def sessions():
-    #flask server calistiginda server adresinde bu sayfa acilir
-    return render_template('index.html')
+@app.get("/")
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        sanal_data(datam1)
+        await asyncio.sleep(0.1)
+        await websocket.send_json(datam1)
 
 if __name__ == '__main__':
-    WSGIServer(('127.0.0.1', 5000), app, handler_class=WebSocketHandler).serve_forever()
+    uvicorn.run(app)
